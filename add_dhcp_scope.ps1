841501655781c88f00a405a3de75426b78f9def4
ammannit <50989557+ammannit@users.noreply.github.com>
2022-06-16 22:48:58 +0200

    Add some parameters

--- add_dhcp_scope.ps1
+++ add_dhcp_scope.ps1
@@ -5,6 +5,12 @@ foreach ($scope in $scopes)
     $name = $scope.name
     $description = $scope.description
     Write-Output "Creating scope  $name"
-    Add-DhcpServerv4Scope -Name "$name" -Description "$description" -StartRange $scope.startrange -EndRange $scope.endrange -SubnetMask $scope.subnetmask -State Active -LeaseDuration 1.00:00:00
-    Set-DhcpServerv4OptionValue -Router $scope.router -ScopeId $scope.scopeid -ComputerName $dhcpserver
+    Add-DhcpServerv4Scope -Name "$name" -Description "$description" -StartRange $scope.startrange -EndRange $scope.endrange -SubnetMask $scope.subnetmask -State Active -LeaseDuration $scope.leaseduration
+    Add-DhcpServerv4ExclusionRange -ScopeId $scope.scopeid  -StartRange  $scope.exclusionstartrange -EndRange $scope.exclusionendrange
+    Add-DhcpServerv4ExclusionRange -ScopeId $scope.scopeid  -StartRange  $scope.exclusionstartrange2 -EndRange $scope.exclusionendrange2
+    Set-DhcpServerv4OptionValue -Router $scope.router -ScopeId $scope.scopeid
+    Remove-DhcpServerv4OptionValue -DnsServer $scope.removednsserver -ScopeId $scope.scopeid
+    Add-DhcpServerv4OptionValue -DnsDomain $scope.dnsdomain -ScopeId $scope.scopeid
+    Add-DhcpServerv4OptionValue -DnsServer $scope.dnsserver -ScopeId $scope.scopeid
+
 }
