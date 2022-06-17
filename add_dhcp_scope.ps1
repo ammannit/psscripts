134f44049a0921a04899fb8de74d1e748a3d4997
ammannit <50989557+ammannit@users.noreply.github.com>
2022-06-17 13:24:54 +0200

    fix some bugs

--- add_dhcp_scope.ps1
+++ add_dhcp_scope.ps1
@@ -1,5 +1,5 @@
 
-$scopes = Import-Csv -Path dhcpserver.csv -Delimiter ";"
+$scopes = Import-Csv -Path C:\Users\amdadm\dhcpserver.csv -Delimiter ";"
 foreach ($scope in $scopes)
 {
     $name = $scope.name
@@ -8,8 +8,7 @@ foreach ($scope in $scopes)
     Add-DhcpServerv4Scope -Name "$name" -Description "$description" -StartRange $scope.startrange -EndRange $scope.endrange -SubnetMask $scope.subnetmask -State Active -LeaseDuration $scope.leaseduration
     Add-DhcpServerv4ExclusionRange -ScopeId $scope.scopeid  -StartRange  $scope.exclusionstartrange -EndRange $scope.exclusionendrange
     Add-DhcpServerv4ExclusionRange -ScopeId $scope.scopeid  -StartRange  $scope.exclusionstartrange2 -EndRange $scope.exclusionendrange2
-    Set-DhcpServerv4OptionValue -Router $scope.router -ScopeId $scope.scopeid
-    Remove-DhcpServerv4OptionValue -DnsServer $scope.removednsserver -ScopeId $scope.scopeid
-    Set-DhcpServerv4OptionValue -DnsDomain $scope.dnsdomain -ScopeId $scope.scopeid
-    Set-DhcpServerv4OptionValue -DnsServer $scope.dnsserver -ScopeId $scope.scopeid
-}
+    Set-DhcpServerv4OptionValue -ScopeId $scope.scopeid -Router $scope.router
+    Set-DhcpServerv4OptionValue -ScopeId $scope.scopeid -DnsDomain $scope.dnsdomain
+    Set-DhcpServerv4OptionValue -ScopeId $scope.scopeid -DnsServer $scope.dnsserver.Split(',')
+} 
